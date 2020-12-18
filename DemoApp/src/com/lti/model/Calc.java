e149fe35a98cda74a127fac5be14808c6e3bdaac
Eshwar Krishnan S <eshwarkrishnan99@gmail.com>
2020-12-18 16:46:21 +0530

    Update Calc.java

--- DemoApp/src/com/lti/model/Calc.java
+++ DemoApp/src/com/lti/model/Calc.java
@@ -3,6 +3,9 @@ package com.lti.model;
 public class Calc {
 
 	public void add(int num1, int num2) {
-		System.out.println("Summation=" + ((num1 - num2)));
+		System.out.println("Summation=" + ((num1 + num2)));
+	}
+	public void prod(int num1, int num2) {
+		System.out.println("Product=" + ((num1 * num2)));
 	}
 }
